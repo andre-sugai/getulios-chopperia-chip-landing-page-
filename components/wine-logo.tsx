8ac806d17f7798cbb316fb06a218b5b7d3e08c89
André Sugai <guitarfreaks@gmail.com>
2026-02-26 16:13:18 -0300

    atualização de imagem

--- components/wine-logo.tsx
+++ components/wine-logo.tsx
@@ -4,7 +4,7 @@ export function WineLogo() {
   return (
     <div className="relative mx-auto h-32 w-32 overflow-hidden rounded-full shadow-xl shadow-black/20 ring-4 ring-amber-700/30 md:h-36 md:w-36">
       <Image
-        src="/images/getulios.webp"
+        src="/images/getulios.jpg"
         alt="Getulios Logo"
         fill
         className="object-cover"
